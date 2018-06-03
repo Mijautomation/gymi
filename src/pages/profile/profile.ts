78091513987466319f48cb1d0bde97b60e787bfa
milansteenwinkel <milanosie@live.nl>
2018-06-03 14:23:41 +0200

    Add Profile Page

---
+++ src/pages/profile/profile.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { User } from '../../models/user';
+import { UserProvider } from '../../providers/user/user';
+
+
+@IonicPage()
+@Component({
+    selector: 'page-profile',
+    templateUrl: 'profile.html',
+})
+export class ProfilePage {
+    user: User;
+
+    constructor(public navCtrl: NavController,
+                public navParams: NavParams,
+                public userProvider: UserProvider,
+                public toastCtrl: ToastController) {
+        if (this.navParams.get('userId')) {
+            this.userProvider.getUserInfo(this.navParams.get('userId'))
+                .subscribe((data) => this.user = data,
+                    (err) => this.handleError(err));
+        }
+        else {
+            this.userProvider.getLoggedInUserInfo()
+                .subscribe((data) => this.user = data,
+                    (err) => this.handleError(err));
+        }
+    }
+
+    private handleError(err) {
+        this.presentToast("Something went wrong with retrieving the data.");
+    }
+
+    private presentToast(message) {
+        let toast = this.toastCtrl.create({
+            message: message,
+            duration: 3000
+        });
+        toast.present();
+    }
+
+
+}
